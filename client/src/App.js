dcdec971b4d8f3b9473a586912f5f27d43b4b616
alexis villafañe <alenegro0512@gmail.com>
2026-04-26 11:38:36 -0300

    Reactivate admin routes in frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import PublicRoutes from './public/PublicRoutes';
+import AdminRoutes from './admin/AdminRoutes'; // Importamos panel de admin
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Deshabilitado en modo demo front-only */}
-        <Route path="/admin/*" element={<Navigate to="/" replace />} />
+        {/* Rutas de Administración */}
+        <Route path="/admin/*" element={<AdminRoutes />} />
         
         {/* Rutas de la web pública */}
         <Route path="/*" element={<PublicRoutes />} />
